ecbfdf93f25e87315ba54050ee24115e92602ac0
Myint Myat <playerone689@gmail.com>
2020-05-16 23:57:29 +0630

    Share Project

---
+++ hello-spring/src/main/java/com/jdc/hello/Hello.java
@@ -0,0 +1,19 @@
+package com.jdc.hello;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Hello {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String message;
+
+}
---
+++ hello-spring/src/main/java/com/jdc/hello/HelloController.java
@@ -0,0 +1,29 @@
+package com.jdc.hello;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+	@Autowired
+	private HelloRepository helloRepository;
+	
+	@GetMapping
+	public List<Hello> getAll(){
+		return helloRepository.findAll();
+	}
+	
+	@PostMapping
+	public Hello create(@RequestBody Hello hello){
+		helloRepository.save(hello);
+		return hello;
+	}
+}
